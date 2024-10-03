3e53c8bcd6b093afd77e2b8b313b9e963f6c4efb
jesus.lozano <jesus.lozano@fundacionctic.org>
2024-10-03 09:16:42 +0200

    Added learning_rate to QNN

--- src/models/quanvolutional_net.py
+++ src/models/quanvolutional_net.py
@@ -24,7 +24,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class QuanvolutionalNet(nn.Module):
     def __init__(self, qkernel_shape=2, classical_kernel_shape=3, embedding=None,
                  circuit=None, measurement=None, params=None, qdevice_kwargs=None,
-                 n_classes=10, batch_size=32, epochs=10):
+                 n_classes=10, batch_size=32, epochs=10, learning_rate=None):
         super(QuanvolutionalNet, self).__init__()
         self.qkernel_shape = qkernel_shape
         self.device = device
@@ -35,6 +35,7 @@ class QuanvolutionalNet(nn.Module):
         self.measurement = measurement
         self.batch_size = batch_size
         self.epochs = epochs
+        self.learning_rate = learning_rate
 
         self.quanv = QuanvLayer(
             qkernel_shape=qkernel_shape,
@@ -90,7 +91,7 @@ class QuanvolutionalNet(nn.Module):
             train_loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
 
         if optimizer is None:
-            optimizer = optim.Adam(self.parameters(), lr=0.001)
+            optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
 
         self.train()
         self.to(self.device)
